128581a7adf1682eb681a7c80e2b5cd2d2678bad
Faraaz Khan <faraaz776@gmail.com>
2026-08-08 10:31:58 +0530

    Debug DB environment variables

--- config/db.js
+++ config/db.js
@@ -10,6 +10,10 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) {
+        console.log('DB_HOST:', process.env.DB_HOST);
+        console.log('DB_PORT:', process.env.DB_PORT);
+        console.log('DB_USER:', process.env.DB_USER);
+        console.log('DB_NAME:', process.env.DB_NAME);
         console.log("Database Connection Failed");
         console.log(err);
     } else {
